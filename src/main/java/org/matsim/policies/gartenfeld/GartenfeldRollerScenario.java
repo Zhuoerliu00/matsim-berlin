9384da06ce932e199ad225a6e44d8a3ca180a8b1
Zhuoer Liu <lzr56195633@163.com>
2025-03-28 08:51:47 +0100

    0328 roller_sharing

--- src/main/java/org/matsim/policies/gartenfeld/GartenfeldRollerScenario.java
+++ src/main/java/org/matsim/policies/gartenfeld/GartenfeldRollerScenario.java
@@ -1,13 +1,23 @@
 package org.matsim.policies.gartenfeld;
 
+//import com.google.inject.AbstractModule;
+import com.google.inject.Provider;
+import com.google.inject.name.Names;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.application.MATSimApplication;
 import org.matsim.contrib.shared_mobility.run.SharingConfigGroup;
+import org.matsim.contrib.shared_mobility.run.SharingModule;
 import org.matsim.contrib.shared_mobility.run.SharingServiceConfigGroup;
 import org.matsim.contrib.shared_mobility.service.SharingUtils;
 import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.RoutingConfigGroup;
 import org.matsim.core.config.groups.ScoringConfigGroup;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.replanning.modules.SubtourModeChoice;
+import org.matsim.core.router.RoutingModule;
+import org.matsim.core.router.TeleportationRoutingModule;
+import org.matsim.core.controler.AbstractModule;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,7 +34,7 @@ public class GartenfeldRollerScenario extends GartenfeldScenario {
 			"--config:controller.runId", "gartenfeld-v6.4.full-roller-1pct",
 			"--config:network.inputNetworkFile", "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/gartenfeld/input/gartenfeld-v6.4.network.xml.gz",
 			"--config:plans.inputPlansFile", "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/gartenfeld/input/gartenfeld-v6.4.population-full-1pct.xml.gz",
-			"--config:controller.lastIteration", "3",
+			"--config:controller.lastIteration", "5",
 			"--config:controller.outputDirectory", "output/gartenfeld-v6.4.full-roller-1pct/"
         );
     }
@@ -37,63 +47,78 @@ public class GartenfeldRollerScenario extends GartenfeldScenario {
 
     @Override
     protected Config prepareConfig(Config config) {
-        config = super.prepareConfig(config);
+		config = super.prepareConfig(config);
 
-		// We need to add the sharing config group
+		// Add sharing config module
 		SharingConfigGroup sharingConfig = new SharingConfigGroup();
 		config.addModule(sharingConfig);
 
-		// Define a service ...
+		// Define the sharing service
 		SharingServiceConfigGroup serviceConfig = new SharingServiceConfigGroup();
+		serviceConfig.setId("roller");
+		serviceConfig.setServiceScheme(SharingServiceConfigGroup.ServiceScheme.StationBased);
+		serviceConfig.setMaximumAccessEgressDistance(2000);
+		serviceConfig.setServiceInputFile("shared_roller_vehicles_stations.xml");
+		serviceConfig.setMode("roller");
+		serviceConfig.setBaseFare(0);
+		serviceConfig.setTimeFare(0.1 / 60);
+
 		sharingConfig.addService(serviceConfig);
 
-		// ... with a service id.
-		serviceConfig.setId("roller");
+		// Register the shared mode as a teleportation mode
+		String sharedMode = SharingUtils.getServiceMode(serviceConfig);
+		RoutingConfigGroup.TeleportedModeParams sharedRoutingParams = new RoutingConfigGroup.TeleportedModeParams(sharedMode);
+		sharedRoutingParams.setTeleportedModeSpeed(5.0);
+		sharedRoutingParams.setBeelineDistanceFactor(1.3);
+		config.routing().addTeleportedModeParams(sharedRoutingParams);
 
-		// ... with StationBased characteristics
-		serviceConfig.setMaximumAccessEgressDistance(100000);
-		serviceConfig.setServiceScheme(SharingServiceConfigGroup.ServiceScheme.StationBased);
-		serviceConfig.setServiceAreaShapeFile(null);
-
-		// ... with a number of available vehicles and their initial locations
-		serviceConfig.setServiceInputFile("shared_roller_vehicles_stations.xml");
-
-		// ... and, we need to define the underlying mode, here "roller".
-		serviceConfig.setMode("roller");
-		serviceConfig.setBaseFare(0.75);    //basis 0.75€
-		serviceConfig.setTimeFare(0.24/60);  //0.24 €/min
-
-		// Finally, we need to make sure that the service mode is considered in mode choice.
+		// Add the shared mode to mode choice
 		List<String> modes = new ArrayList<>(Arrays.asList(config.subtourModeChoice().getModes()));
-		//modes.add(SharingUtils.getServiceMode(serviceConfig));
-		modes.add(removeSharingPrefix(SharingUtils.getServiceMode(serviceConfig)));
-		config.subtourModeChoice().setModes(modes.toArray(new String[modes.size()]));
+		modes.add(sharedMode);
+		config.subtourModeChoice().setModes(modes.toArray(new String[0]));
 
-		// We need to add interaction activity types to scoring
-		ScoringConfigGroup.ActivityParams pickupParams = new ScoringConfigGroup.ActivityParams(SharingUtils.PICKUP_ACTIVITY);
-		pickupParams.setScoringThisActivityAtAll(false);
-		config.scoring().addActivityParams(pickupParams);
 
-		ScoringConfigGroup.ActivityParams dropoffParams = new ScoringConfigGroup.ActivityParams(SharingUtils.DROPOFF_ACTIVITY);
-		dropoffParams.setScoringThisActivityAtAll(false);
-		config.scoring().addActivityParams(dropoffParams);
+		// Add activity types used in shared mobility
+		for (String act : List.of(SharingUtils.PICKUP_ACTIVITY, SharingUtils.DROPOFF_ACTIVITY, SharingUtils.BOOKING_ACTIVITY)) {
+			ScoringConfigGroup.ActivityParams params = new ScoringConfigGroup.ActivityParams(act);
+			params.setScoringThisActivityAtAll(false);
+			config.scoring().addActivityParams(params);
+		}
 
-		ScoringConfigGroup.ActivityParams bookingParams = new ScoringConfigGroup.ActivityParams(SharingUtils.BOOKING_ACTIVITY);
-		bookingParams.setScoringThisActivityAtAll(false);
-		config.scoring().addActivityParams(bookingParams);
+		// Score base mode (roller)
+		ScoringConfigGroup.ModeParams rollerParams = new ScoringConfigGroup.ModeParams("sharing_roller");
+		config.scoring().addModeParams(rollerParams);
 
-		// We need to score roller
-		ScoringConfigGroup.ModeParams rollerScoringParams = new ScoringConfigGroup.ModeParams("roller");
-		config.scoring().addModeParams(rollerScoringParams);
-
-        return config;
+		return config;
     }
 
     @Override
     protected void prepareControler(Controler controler) {
         super.prepareControler(controler);
+		// Important: register SharingModule which sets up routing, scoring, QSim components, etc.
+		controler.addOverridingModule(new SharingModule());
+
+		controler.addOverridingModule(new AbstractModule() {
+			@Override
+			public void install() {
+				bind(RoutingModule.class)
+					.annotatedWith(Names.named("roller"))
+					.toProvider(() -> new TeleportationRoutingModule(
+						"sharing_roller",
+						controler.getScenario(),
+						5.0, // speed
+						1.3  // beeline distance factor
+					));
+			}
+		});
+
+		// Add shared mobility QSim components
+		SharingConfigGroup sharingConfig = ConfigUtils.addOrGetModule(controler.getConfig(), SharingConfigGroup.class);
+		controler.configureQSimComponents(SharingUtils.configureQSim(sharingConfig));
 
     }
+
+
 
 	public static String removeSharingPrefix(String mode) {
 		return mode.replace("sharing_", "");
